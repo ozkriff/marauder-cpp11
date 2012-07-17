eae003b426442598902d0a471df0b0bc46e97b83
ozkriff <ozkriff@gmail.com>
2012-07-17 11:34:46 +0400

    path.h: Removed todo-comments

--- path.h
+++ path.h
@@ -2,9 +2,5 @@
 
 void init_pathfinding_module(void);
 void get_path(List *path, V2i pos);
-
-/* TODO remove from public functions. */
 void fill_map(const Unit *u);
-
-/* TODO remove from public functions. */
 void clean_map(void);
