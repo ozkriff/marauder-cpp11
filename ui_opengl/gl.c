731bea10b74bff55807e254400b3456b742456ab
ozkriff <ozkriff@gmail.com>
2012-08-09 17:43:15 +0400

    ui_opengl/gl: Refactored

--- ui_opengl/gl.c
+++ ui_opengl/gl.c
@@ -9,54 +9,64 @@
 #include "core/misc.h"
 #include "ui_opengl/gl.h"
 
+static GLenum get_texture_format(
+    const SDL_Surface *surface, int n_of_colors)
+{
+  if (n_of_colors == 4) {
+    /* contains an alpha channel */
+    if (surface->format->Rmask == 0xff) {
+      return GL_RGBA;
+    } else {
+      return GL_BGRA;
+    }
+  } else if (n_of_colors == 3) {
+    /* no alpha channel */
+    if (surface->format->Rmask == 0xff) {
+      return GL_RGB;
+    } else {
+      return GL_BGR;
+    }
+  } else {
+    die("gl.c: load_texture(): "
+        "the image is not truecolor.."
+        "  this will probably break\n");
+    return 0;
+  }
+}
+
+static void set_texture_parameters(void) {
+  glTexParameteri(GL_TEXTURE_2D,
+      GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D,
+      GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D,
+      GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D,
+      GL_TEXTURE_MIN_FILTER,
+      GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D,
+      GL_GENERATE_MIPMAP, GL_TRUE);
+}
+
+static bool is_power_of_two(int n) {
+  return ((n & (n - 1)) == 0);
+}
+
 bool load_texture(GLuint *id, const char *filename) {
   GLenum texture_format;
   GLint n_of_colors;
   SDL_Surface *surface;
   assert(id);
   surface = IMG_Load(filename);
+  /* TODO: if (!surface) { die(); } */
   if (surface) {
-    if ((surface->w & (surface->w - 1)) != 0) {
-      die("gl.c: load_texture(): "
-          "image's width is not a power of 2\n");
-    }
-    if ((surface->h & (surface->h - 1)) != 0) {
-      die("gl.c: load_texture(): "
-          "image's height is not a power of 2\n");
-    }
+    assert(is_power_of_two(surface->w));
+    assert(is_power_of_two(surface->h));
     n_of_colors = surface->format->BytesPerPixel;
-    if (n_of_colors == 4) {
-      /* contains an alpha channel */
-      if (surface->format->Rmask == 0x000000ff) {
-        texture_format = GL_RGBA;
-      } else {
-        texture_format = GL_BGRA;
-      }
-    } else if (n_of_colors == 3) {
-      /* no alpha channel */
-      if (surface->format->Rmask == 0x000000ff) {
-        texture_format = GL_RGB;
-      } else {
-        texture_format = GL_BGR;
-      }
-    } else {
-      die("gl.c: load_texture(): "
-          "the image is not truecolor.."
-          "  this will probably break\n");
-    }
+    texture_format = get_texture_format(surface, n_of_colors);
     glGenTextures(1, id);
     glBindTexture(GL_TEXTURE_2D, *id);
-    glTexParameteri(GL_TEXTURE_2D,
-        GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,
-        GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,
-        GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,
-        GL_TEXTURE_MIN_FILTER,
-        GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,
-        GL_GENERATE_MIPMAP, GL_TRUE);
+    set_texture_parameters();
     glTexImage2D(GL_TEXTURE_2D, 0, n_of_colors,
         surface->w, surface->h, 0,
         texture_format, GL_UNSIGNED_BYTE, surface->pixels);
