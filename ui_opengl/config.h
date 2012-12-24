f2216e8ef9a50ca85699ec1bc66837e3a82519a4
ozkriff <ozkriff@gmail.com>
2012-12-24 13:10:44 +0400

    ui/config: Increased resolution

--- ui_opengl/config.h
+++ ui_opengl/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 #define DATA_DIR "/home/ozkriff/projects/my/rusty_armies/data/"
-#define WIN_WIDTH 300
-#define WIN_HEIGHT 200
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
 
 #define DEFAULT_FONT \
   "/usr/share/fonts/truetype/liberation/" \
