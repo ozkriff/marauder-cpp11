e11c566349fac17c31268f00526f665578c20978
ozkriff <ozkriff@gmail.com>
2012-12-24 13:08:10 +0400

    ui_opengl/config.h: Made DATA_DIR full path

--- ui_opengl/config.h
+++ ui_opengl/config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#define DATA_DIR "../data/"
+#define DATA_DIR "/home/ozkriff/projects/my/rusty_armies/data/"
 #define WIN_WIDTH 300
 #define WIN_HEIGHT 200
 
