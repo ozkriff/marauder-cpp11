182be3e468e4871b54d00389539b8f2c8f94dba7
ozkriff <ozkriff@gmail.com>
2013-01-08 02:22:04 +0400

    Extructed pathQueue from path.{cpp,h}

--- core/path.h
+++ core/path.h
@@ -5,25 +5,10 @@
 
 #include <vector>
 #include "core/dir.h"
+#include "core/pathQueue.h"
 
 class Unit;
 class Core;
-
-class PathQueue {
-private:
-  Core& mCore;
-  unsigned int mTailNodeIndex;
-  unsigned int mHeadNodeIndex;
-  std::vector<V2i> mNodes;
-
-public:
-  PathQueue(Core& core, int size);
-  ~PathQueue();
-
-  bool isEmpty() const;
-  void push(const V2i& m, Dir parent, int newcost, Dir dir);
-  V2i pop();
-};
 
 class Pathfinder {
 private:
---
+++ core/pathQueue.h
@@ -0,0 +1,27 @@
+// See LICENSE file for copyright and license details.
+
+#ifndef CORE_PATHQUEUE_H
+#define CORE_PATHQUEUE_H
+
+#include <vector>
+#include "core/dir.h"
+
+class Core;
+
+class PathQueue {
+private:
+  Core& mCore;
+  unsigned int mTailNodeIndex;
+  unsigned int mHeadNodeIndex;
+  std::vector<V2i> mNodes;
+
+public:
+  PathQueue(Core& core, int size);
+  ~PathQueue();
+
+  bool isEmpty() const;
+  void push(const V2i& m, Dir parent, int newcost, Dir dir);
+  V2i pop();
+};
+
+#endif
