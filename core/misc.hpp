cb35557406bceebeda789923b29528483b834d44
ozkriff <ozkriff@gmail.com>
2013-02-08 15:12:40 +0400

    core/misc.hpp: Fixed getNext(..)

--- core/misc.hpp
+++ core/misc.hpp
@@ -3,14 +3,20 @@
 #ifndef CORE__MISC_HPP
 #define CORE__MISC_HPP
 
+#include <cassert>
 #include <list>
 
+// TODO: Rewrite :(
 template <class T>
 T* getNext(std::list<T*> &list, T* node) {
+  assert(node);
   auto it = list.begin();
   while (it != list.end()) {
     if (*it == node) {
       ++it;
+      if (it == list.end()) {
+        return nullptr;
+      }
       return *it;
     }
     ++it;
